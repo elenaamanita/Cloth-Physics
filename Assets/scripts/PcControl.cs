55e893d423f63d4b2fdfd53561994da33beb9973
Eleni <elenipaskali@yahoo.com>
2016-05-17 18:26:48 +0100

    Pc Control

---
+++ Assets/scripts/PcControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PcControl : MonoBehaviour {
+
+    private Rigidbody bd;
+
+    [SerializeField]
+    private float Speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        bd = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float depth = 0.0f;
+        if(Input.GetKey(KeyCode.Z))
+           depth = 1.0f;
+        else if (Input.GetKey(KeyCode.X))
+            depth = -1.0f;
+
+        bd.velocity = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            depth) * Time.deltaTime * Speed;
+	
+	}
+}
